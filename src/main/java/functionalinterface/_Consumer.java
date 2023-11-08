829d7925bfd0b4186599926c06547ec59ad704dd
Eria Othieno Pinyi <pinyito@users.noreply.github.com>
2023-11-07 23:47:46 -0600

    implemented BiConsumer

--- src/main/java/functionalinterface/_Consumer.java
+++ src/main/java/functionalinterface/_Consumer.java
@@ -1,5 +1,6 @@
 package functionalinterface;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public class _Consumer {
@@ -7,16 +8,22 @@ public class _Consumer {
        Customer customer = new Customer("Maria", "6412332403");
         // Normal Java function: consumer
         greetCustomer(customer);
+        greetCustomerV2(customer, false);
 
         // Functional interface: Consumer
         greetCustomerConsumer.accept(customer);
+        greetCustomerBiConsumer.accept(customer, false);
 
     }
 
     static Consumer<Customer> greetCustomerConsumer = customer -> System.out.println(String.format("Hello %s thanks for registering phone number %s", customer.customerName, customer.customerPhoneNumber));
+    static BiConsumer<Customer, Boolean> greetCustomerBiConsumer = (customer, showPhoneNumber) -> System.out.println(String.format("Hello %s thanks for registering phone number %s", customer.customerName, (showPhoneNumber? customer.customerPhoneNumber:"**********")));
 
     static void greetCustomer(Customer customer){
         System.out.println(String.format("Hello %s thanks for registering phone number %s", customer.customerName, customer.customerPhoneNumber));
+    }
+    static void greetCustomerV2(Customer customer, Boolean showPhoneNumber){
+        System.out.println(String.format("Hello %s thanks for registering phone number %s", customer.customerName, (showPhoneNumber? customer.customerPhoneNumber:"**********")));
     }
 
     static class Customer {
